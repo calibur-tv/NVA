a315fc3aba027877ea1a190c122e67749be90a9e
falstack <icesilt@outlook.com>
2018-12-04 17:49:21 +0800

    remove: my code

--- pages/error/404.vue
+++ pages/error/404.vue
@@ -21,12 +21,6 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">版权所有
-          <a
-            class="link-type"
-            href="https://cn.vuejs.org/"
-            target="_blank">vue.js</a>
-        </div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
         <a
